0a49a11b1d18ea603799b14eab872ffb797292fb
lucaskokob <lucaskokob@hotmail.com>
2026-05-18 11:23:32 +0200

    Added database for books and created get request for them

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ const app = express();
 const PORT = 3000;
 app.use(express.json());
 
+const db = new sqlite3.Database("./books.db");
+
+db.run(
+  "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, author TEXT, borrowed BOOLEAN DEFAULT FALSE)"
+);
+
+app.get("/books", (req, res) => {
+  db.all("SELECT * FROM books", (err, rows) => {
+    if (rows.length === 0) {
+      res.status(204).json({ message: "No books found" });
+      return;
+    }
+    res.json(rows);
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
